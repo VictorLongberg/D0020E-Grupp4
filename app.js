e33cc5db2ad648613dccd11589414eeeeb68a9ac
PontusNorrby <pontus.norrby@hotmail.se>
2021-02-02 11:10:38 +0100

    test

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 // code from https://socket.io/get-started/chat/
-var app = require('express')();
+// code from https://socket.io/get-started/chat/
+
+//var app = require('express')();
+const express = require('express');
+const app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http)
 const port = 3000
 
 var questionCounter = 0;
+var memberCounter = 0;
+
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/client/index.html');
+	res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', (socket) => {
-	console.log('user connected')
+	memberCounter++;
+	console.log('user connected, total users: ' +memberCounter);
+
 	socket.on('disconnect', () => {
-		console.log('user disconnected');
+		console.log('user disconnected, total users: ' +memberCounter);
+		memberCounter--;
 	});
 
 	socket.on('chat message', (sender, msg) => {
@@ -26,10 +37,10 @@ io.on('connection', (socket) => {
 		io.emit('question', sender, msg, questionCounter);
 		questionCounter++;
 	});
-	
-	socket.on('upvote', (questionID) => {
-		console.log('question ' +questionID +'was upvoted');
-		io.emit('upvote', questionID);
+
+	socket.on('upvote', (questionID, memberNum) => {
+		console.log('question ' +questionID +' was upvoted');
+		io.emit('upvote', questionID, memberCounter);
 	});
 });
 
