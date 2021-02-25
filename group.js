ffb2605bc66bad664743da5974dce64bd5e29773
Erik Serrander <s7rul@ludd.ltu.se>
2021-02-25 15:04:02 +0100

    thing

---
+++ group.js
@@ -0,0 +1,30 @@
+/** Class representing a Student. */
+class Group {
+
+	constructor(name) {
+		this.name = name;
+		this.member_list = [];
+	}
+
+	get_socket_list() {
+		var ret = [];
+		for (var i = 0; i < this.member_list.lenght; i++){
+			ret.push(this.member_list[i].get_socket();
+		}
+		return ret;
+	}
+
+	add_student(stud) {
+		this.member_list.push(stud);
+	}
+	
+	remove_student(id) {
+		for (var i = 0; i < this.member_list.lenght; i++){
+			if (id == this.member_list[i].get_id()){
+				this.member_list.splice(i, 1);
+			}
+		}
+	}
+}
+
+module.exports = {Group};
